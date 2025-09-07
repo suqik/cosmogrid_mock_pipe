aa45590c63f4395b8149febec519e39c0e8630f0
suqik <suqikuai777@gmail.com>
2025-09-07 20:30:09 +0800

    update running script

--- run_make_fore_void.sh
+++ run_make_fore_void.sh
@@ -2,7 +2,7 @@
 #SBATCH -J make_fore_void
 #SBATCH -p master
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=64
 #SBATCH --cpus-per-task=1
 #SBATCH --hint=nomultithread
 #SBATCH -o %J.out
@@ -10,4 +10,4 @@
 
 export OMP_NUM_THREADS=1
 
-pixi run python make_fore_void.py
+pixi run mpirun -np 64 --map-by core --bind-to core python make_fore_void.py
