a70ba18798c39419f9a9f26dddaf503f302df908
suqik <suqikuai777@gmail.com>
2025-09-07 20:29:55 +0800

    support mpi

--- make_fore_void.py
+++ make_fore_void.py
@@ -10,17 +10,14 @@ from loguru import logger
 from utils.io_func import *
 from utils.mkfore_utils import *
 
-''' Setup '''
+''' cosmology, HOD and galaxy catalog info '''
 sim_fmt = "/data3/suchen/CosmoGridV1/grid/cosmo_{:06d}/run_0/"
-halo_fmt = "pkd_halos/CosmoML.{:05d}.fofstats.0"
-redshift_label = 120 # corresponding to z~0.3
+hod_param_fname = "cfgs/hod/hod_5params_dict.json"
+gfmt = "/data2/suchen/CosmoGrid/HOD/cosmo_{:06d}_run_0_HOD_{}_run_0_boss_north_2dflens_south.npy"
 
-lb_z_file = "/data3/suchen/CosmoGridV1/label_z_table.txt"
-lb_z_tb = np.loadtxt(lb_z_file)
+''' Output info '''
+vfmt = "/data2/suchen/CosmoGrid/Void/cosmo_{:06d}_run_0_HOD_{}_run_0_boss_north.npy"
 
-Lbox = 900.0
-Nside = 832 # Npart = Nside**3
-redshift = lb_z_tb[redshift_label,1]
 ### FIXME: for test
 zmin = 0.2
 zmax = 0.4
@@ -47,6 +44,23 @@ mask_boss_fname_list = [
 
 mask_weight_2df_fname = "catalogs/masks/2dflens_geom/2dFLens_mask_weight.fits"
 
+def gal2void(tracer_pos_cart, cosmo_ccl, survey:int, rank=0):
+    void_pos_cart, void_radii = find_void(tracer_pos_cart, 
+                                    dive_input='tmp/tmp_tracer_{}.dat'.format(rank), 
+                                    dive_output = 'tmp/tmp_void_{}.dat'.format(rank))
+    void_ra, void_dec, void_z, phys_cut = Cart2Sph(cosmo_ccl, pos=void_pos_cart)
+    void_radii = void_radii[phys_cut]
+    
+    void_lcone = np.empty(len(void_ra), dtype=fvoid_type)
+    void_lcone['ra'] = void_ra
+    void_lcone['dec'] = void_dec
+    void_lcone['z'] = void_z
+    void_lcone['Rv'] = void_radii
+    void_lcone['w'] = 1.0
+    void_lcone['survey'] = survey
+
+    return void_lcone
+
 def find_voids_with_boundary_effect(galcone:np.ndarray, 
                                     geoms:dict, masks:dict, 
                                     cosmo_ccl:ccl.Cosmology, 
@@ -59,16 +73,18 @@ def find_voids_with_boundary_effect(galcone:np.ndarray,
     ### find voids, and transform to Spherical coordinates
     logger.info("Find voids")
 
-    void_pos_cart, void_radii = find_void(gal_pos_cart)
-    void_ra, void_dec, void_z, phys_cut = Cart2Sph(cosmo_ccl, pos=void_pos_cart)
-    void_radii = void_radii[phys_cut]
-    
-    void_lcone = np.empty(len(void_ra), dtype=fvoid_type)
-    void_lcone['ra'] = void_ra
-    void_lcone['dec'] = void_dec
-    void_lcone['z'] = void_z
-    void_lcone['Rv'] = void_radii
-    void_lcone['w'] = np.ones(len(void_ra))
+    ### BOSS CMASSLOWZ
+    logger.info("LOWZCMASS")
+    select = galcone['survey'] == 0
+    void_lcone = gal2void(gal_pos_cart[select], cosmo_ccl, 0, rank)
+    ### BOSS LOWZE2
+    logger.info("LOWZE2")
+    select = galcone['survey'] == 1
+    void_lcone = np.append(void_lcone, gal2void(gal_pos_cart[select], cosmo_ccl, 1, rank))
+    ### BOSS LOWZE3
+    logger.info("LOWZE3")
+    select = galcone['survey'] == 2
+    void_lcone = np.append(void_lcone, gal2void(gal_pos_cart[select], cosmo_ccl, 2, rank))
 
     ### apply redshift cut
     zcut = (void_lcone['z'] >= zmin) & (void_lcone['z'] <= zmax)
@@ -78,38 +94,59 @@ def find_voids_with_boundary_effect(galcone:np.ndarray,
     logger.info("Apply survey geometry cut")
     ## CMASSLOWZ
     logger.info("LOWZCMASS")
-    boss_lowzcmass_void, _ = apply_boss_geometry(void_lcone, geoms['lowzcmass'], masks, galcone_ids=None)
+    select = void_lcone['survey'] == 0
+    boss_lowzcmass_void, _ = apply_boss_geometry(void_lcone[select], geoms['lowzcmass'], masks, galcone_ids=None)
     ## LOWZE2
     logger.info("LOWZE2")
-    boss_lowze2_void, _ = apply_boss_geometry(void_lcone, geoms['lowze2'], masks, galcone_ids=None)
-    boss_lowze2_void, _ = apply_boss_lowze2e3_trim(void_lcone, geoms['lowz'], galcone_ids=None)
+    select = void_lcone['survey'] == 1
+    boss_lowze2_void, _ = apply_boss_geometry(void_lcone[select], geoms['lowze2'], masks, galcone_ids=None)
+    boss_lowze2_void, _ = apply_boss_lowze2e3_trim(boss_lowze2_void, geoms['lowz'], galcone_ids=None)
     ## LOWZE3
     logger.info("LOWZE3")
-    boss_lowze3_void, _ = apply_boss_geometry(void_lcone, geoms['lowze3'], masks, galcone_ids=None)
-    boss_lowze3_void, _ = apply_boss_lowze2e3_trim(void_lcone, geoms['lowz'], galcone_ids=None)
+    select = void_lcone['survey'] == 2
+    boss_lowze3_void, _ = apply_boss_geometry(void_lcone[select], geoms['lowze3'], masks, galcone_ids=None)
+    boss_lowze3_void, _ = apply_boss_lowze2e3_trim(boss_lowze3_void, geoms['lowz'], galcone_ids=None)
 
-    return boss_lowzcmass_void, boss_lowze2_void, boss_lowze3_void
+    void_lcone = np.append(boss_lowzcmass_void, boss_lowze2_void)
+    void_lcone = np.append(void_lcone, boss_lowze3_void)
+
+    return void_lcone
 
 if __name__ == "__main__":
-    cosmo_label = 1
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
 
-    cpar_file = sim_fmt.format(cosmo_label) + "params.yml"
-    cosmo_ccl = get_cosmo_from_file(cpar_file, otype='ccl')
+    if rank == 0:
+        
+        logger.info("Read cosmology and HOD parameters")
 
-    gdir = "/data2/suchen/CosmoGrid/HOD/"
-    # TODO: change to npy
-    ifmt = "cosmo_{:06d}_run_0_HOD_0_run_0_boss_north_2dflens_south.txt"
-    gfile = gdir + ifmt.format(cosmo_label)
+        hod_params_dict = get_hod_params(hod_param_fname)
+        cosmo_labels_tot = []
 
-    vdir = "/data2/suchen/CosmoGrid/Void/"
-    ofmt = "cosmo_{:06d}_run_0_HOD_0_run_0_{}.npy"
+         for icosmo_str in hod_params_dict.keys():
+             if len(hod_params_dict[icosmo_str]) > 0:
+                 cosmo_labels_tot.append(int(icosmo_str[5:]))
 
-    logger.info("Load galaxy catalog")
-    # TODO: change to npy
-    galcone = np.loadtxt(gfile, dtype=fgal_type)
-    ## boss galaxy
-    boss_cut = galcone['dec'] > -10
-    galcone = galcone[boss_cut]
+        # ############# read legacy cosmology #############
+        # with open("/data2/suchen/CosmoGrid/diff_cosmo.txt", "r") as f:
+        #     cosmo_labels_tot = [int(line.split('_')[1]) for line in f.readlines()]
+        # #################################################
+
+        k, m = divmod(len(cosmo_labels_tot), size)
+        clb_chunks_tbs = [cosmo_labels_tot[i*k + min(i,m) : (i + 1)*k + min(i+1, m)] for i in range(size)]
+        chunks = []
+        for ichunk in clb_chunks_tbs:
+            hod_dict_tbs = {}
+            for icosmo in ichunk:
+                hod_dict_tbs["cosmo{:06d}".format(icosmo)] = hod_params_dict["cosmo{:06d}".format(icosmo)]
+            chunks.append(hod_dict_tbs)
+    
+    else:
+        chunks = None
+
+    hod_param_dict_local = comm.scatter(chunks, root=0)
 
     logger.info("Load mask files")
 
@@ -123,10 +160,34 @@ if __name__ == "__main__":
     for mask_fname in mask_boss_fname_list:
         masks.append(pymangle.Mangle(mask_fname))
 
-    boss_lowzcmass_void, boss_lowze2_void, boss_lowze3_void = find_voids_with_boundary_effect(galcone, geoms, masks, cosmo_ccl, zmin, zmax)
+    logger.info("Main process")
 
-    logger.info("Save void catalog")
+    for icosmo_str in hod_param_dict_local.keys():
+        cosmo_label = int(icosmo_str[5:])
+        for ihod in range(len(hod_param_dict_local[icosmo_str])):
+            
+            logger.info("Load Cosmology")
+            
+            cpar_file = sim_fmt.format(cosmo_label) + "params.yml"
+            cosmo_ccl = get_cosmo_from_file(cpar_file, otype='ccl')
 
-    np.save(vdir + ofmt.format(cosmo_label, "lowzcmass"), boss_lowzcmass_void)
-    np.save(vdir + ofmt.format(cosmo_label, "lowze2"), boss_lowze2_void)
-    np.save(vdir + ofmt.format(cosmo_label, "lowze3"), boss_lowze3_void)
+            logger.info("Load galaxy catalog")
+
+            gfile = gfmt.format(cosmo_label, ihod)
+
+            vdir = "/data2/suchen/CosmoGrid/Void/"
+            ofmt = "cosmo_{:06d}_run_0_HOD_0_run_0_{}.npy"
+
+            logger.info("Load galaxy catalog")
+
+            galcone = np.load(gfile)
+            ## boss galaxy
+            boss_cut = galcone['survey'] != 3
+            galcone = galcone[boss_cut]
+
+            boss_lowzcmasstot_void = find_voids_with_boundary_effect(galcone, geoms, masks, cosmo_ccl, zmin, zmax)
+
+            logger.info("Save void catalog")
+
+            vfile = vfmt.format(cosmo_label, ihod)
+            np.save(vfile, boss_lowzcmasstot_void)
