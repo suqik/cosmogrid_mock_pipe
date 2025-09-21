e6f8dfbcaba85e4d53b7ae9840535e94184ba334
suqik <suqikuai777@gmail.com>
2025-09-21 14:04:08 +0800

    add run fcfc script

--- aux/submit.s
+++ aux/submit.s
@@ -2,11 +2,11 @@
 #SBATCH -J fcfc
 #SBATCH -p master
 #SBATCH -N 1
-#SBATCH -n 32
+#SBATCH -n 4
 #SBATCH --hint=nomultithread
 #SBATCH -o %J.out
 #SBATCH -e %J.err
 
 export OMP_NUM_THREADS=1
 
-mpirun -n 32 --map-by core --bind-to core ~/applications/FCFC_v1_1_0_mpi/FCFC_2PT -c cfgs/fcfc_2pt.conf
+mpirun -n 4 --map-by core --bind-to core ~/applications/FCFC_v1_1_0_mpi/FCFC_2PT -c cfgs/fcfc_2pt_vg.conf
